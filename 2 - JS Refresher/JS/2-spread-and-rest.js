40fcfbb520696351c49dd19a40ed9c94e6eaac3f
Gleb Helgeson <glebh@live.no>
2022-04-16 00:03:24 +0200

    complete pt 2-rest-and-spread

--- 2 - JS Refresher/JS/2-spread-and-rest.js
+++ 2 - JS Refresher/JS/2-spread-and-rest.js
@@ -1,7 +1,29 @@
 let spreadAndRest = () => {
+    /* #region Arrays */
     let numbers = [1,2,3,4];
     console.log('numbers:', numbers);
     console.log('numbers = [...numbers, 5,6,7]');
     numbers = [...numbers, 5, 6, 7];
     console.log('numbers:', numbers);
+    /* #endregion */
+
+    /* #region objects */
+    let firstName = {
+        name: "Gleb"
+    }
+    console.log('firstName: ', firstName);
+    let fullName = {
+        ...firstName,
+        lastName: "Belg"
+    }
+    console.log('fullName: ', fullName);
+    /* #endregion */
+
+    /* #region rest */
+    let restFunc = (...inputVals) => {
+        // inputVals gets put into an array
+        return inputVals.filter(x => x%2==0);
+    }
+    console.log(restFunc(1,2,3,4));
+    /* #endregion */
 }
