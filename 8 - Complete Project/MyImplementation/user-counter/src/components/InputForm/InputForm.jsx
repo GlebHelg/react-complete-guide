0e7199fa5cd25acbe805b5215213a115a21f7c6c
Gleb Helgeson <glebh@live.no>
2022-04-17 23:58:12 +0200

    8 - myImplementation - implement addFormElementsToParentState

--- 8 - Complete Project/MyImplementation/user-counter/src/components/InputForm/InputForm.jsx
+++ 8 - Complete Project/MyImplementation/user-counter/src/components/InputForm/InputForm.jsx
@@ -13,14 +13,22 @@ const GenerateHtmlFromInputList = (inputFieldsList) => {
 const collectFormValues = (e) => {
     e.preventDefault();
 
+    // normal stuff did not work, please explore how this works if you wish in browser
     const formProps = Object.keys(e.target)
                             .slice(0, e.target.length)
                             .map(idx => e.target[idx]);
-    
-    const formObjects = formProps.map(x => { return {'id: ': x.id, 'val: ': x.value}; })
+    const formObjects = formProps.map(x => { return {'id': x.id, 'val': x.value}; })
                                  .slice(0, formProps.length-1);
 
+    return formObjects;
 }
+
+const addFormElementsToUserCounterList = (e, setUserList) => {
+    e.preventDefault();
+
+    const formValues = collectFormValues(e);
+    setUserList(formValues);
+};
 
 const InputForm = (props) => {
     console.log(props);
@@ -28,7 +36,7 @@ const InputForm = (props) => {
 
     return (
         // onSubmit triggers rerender...
-        <form className="input-form" id="input-form" onSubmit={(e) => collectFormValues(e)}>
+        <form className="input-form" id="input-form" onSubmit={(e) => addFormElementsToUserCounterList(e, props.parentSetUserList)}>
             {inputFieldsListJsx}
         </form>
     );
--- 8 - Complete Project/MyImplementation/user-counter/src/components/UserCounterMain/UserCounterMain.jsx
+++ 8 - Complete Project/MyImplementation/user-counter/src/components/UserCounterMain/UserCounterMain.jsx
@@ -1,4 +1,5 @@
 import InputForm from '../InputForm/InputForm';
+import {useState} from 'react';
 
 const inputFormPropsGenerator = () => {
 
@@ -24,11 +25,18 @@ const inputFormPropsGenerator = () => {
 
 const UserCounterMain = () => {
 
+    let [userList, setUserList] = useState([]);
+
+    console.log('userList: ', userList);
     return (
         <div>
             <h1>User Counter Main</h1>
-            <InputForm inputFieldsList={inputFormPropsGenerator()}/>
-
+            <InputForm inputFieldsList={inputFormPropsGenerator()} 
+                       parentSetUserList={setUserList}/>
+            <p>{userList.length > 1 ? userList[0].id  : ''}</p>
+            <p>{userList.length > 1 ? userList[0].val : ''}</p><br />
+            <p>{userList.length > 1 ? userList[1].id  : ''}</p>
+            <p>{userList.length > 1 ? userList[1].val : ''}</p><br />
         </div>
 
     );
