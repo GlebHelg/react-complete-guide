8385fd5f38e44fee9e67b9a991aba0ad4d45dc0f
Gleb Helgeson <glebh@live.no>
2022-04-18 10:57:16 +0200

    8 - myImplementation - style FromDataRenderer more, add remove row btn

--- 8 - Complete Project/MyImplementation/user-counter/src/components/FormDataRenderer/FormDataRenderer.jsx
+++ 8 - Complete Project/MyImplementation/user-counter/src/components/FormDataRenderer/FormDataRenderer.jsx
@@ -4,23 +4,28 @@ import './FormDataRenderer.css';
 
 const FormDataRenderer = (props) => {
 
-    const renderData = props.formData.map(formObject => 
-       <div style={{border: '5px solid #999999', display:'block'}} >
-           {formObject.map(formField => {
-               return (
-                   <div className="renderFormRow">
-                    <p className="renderFormCol">{formField.id}</p>
-                    <p className="renderFormCol">{formField.val}</p>
-                   </div>
-               );
-           })}
-       </div> 
+    const renderData = props.formData.map((formObject,idx) => 
+        <div className="form-data-and-btn-wrapper">
+            <div className="form-data-wrapper" >
+                {formObject.map(formField => {
+                    return (
+                        <div className="renderFormRow">
+                            <p className="renderFormCol">{formField.id}</p>
+                            <p className="renderFormCol">{formField.val}</p>
+                        </div>
+                    );
+                })}
+            </div> 
+            <button className="remove-me-btn" onClick={() => console.log('remove row with idx: ', idx)}>
+                X
+            </button>
+        </div>
     );
     
     return (
-        <div style={{border: '5px solid #999999', width:'50%', display:'inline-block', textAlign: 'center'}}>
-            {renderData}
-        </div>
+            <div className="form-data-renderer">
+                {renderData}
+            </div>
     );
 }
 export default FormDataRenderer;
