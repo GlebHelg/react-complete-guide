31b2c30fa98a005647c5d9a43607ff9b4fec49d0
Gleb Helgeson <glebh@live.no>
2022-04-18 14:10:31 +0200

    8 - myImplementation - quick and dirty but complete and working. Cleaning and better styling remains.

--- 8 - Complete Project/MyImplementation/user-counter/src/components/InputForm/AddFormElement/AddFormElement.jsx
+++ 8 - Complete Project/MyImplementation/user-counter/src/components/InputForm/AddFormElement/AddFormElement.jsx
@@ -1,22 +1,46 @@
 import {useState} from 'react';
 
-const addFormFieldElement = (e) => {
-    console.log("addFormFieldElement e:", e);
+const collectFormValues = (e) => {
+    e.preventDefault();
+
+    // normal stuff did not work, please explore how this works if you wish in browser
+    const formProps = Object.keys(e.target)
+                            .slice(0, e.target.length)
+                            .map(idx => e.target[idx]);
+    const formObjects = formProps.map(x => { return {'id': x.id, 'val': x.value}; })
+                                 .slice(0, formProps.length-1);
+
+    return formObjects;
 }
 
-const AddFormElement = () => {
+const addFormFieldElement = (e, formFields, setFormFields) => {
+    e.preventDefault();
 
+    const formValues = collectFormValues(e);
+    const formObject = {
+        inputType: formValues[0].val,
+        inputLabel: formValues[1].val,
+        inputId: formValues[1].val
+    };
+
+    formFields.splice(formFields.length-1, 0, formObject);
+
+    setFormFields([...formFields]);
+}
+
+const AddFormElement = (props) => {
 
     return (
         <form
-            onSubmit={(e) => addFormFieldElement(e)}>
+            onSubmit={(e) => addFormFieldElement(e, props.formFields, props.setFormFields)}>
             <div>Add Field</div>
             <label htmlFor="addFormInputType">Type:</label>
             <select id="addFormInputType">
                 <option>text</option>
                 <option>number</option>
             </select>
-            <input id="someid" type="text" placeholder='enter some id '/>;
+            <label htmlFor="someid">Id:</label>
+            <input id="someid" type="text" placeholder='enter some id '/>
             <input type="submit" value="Add"/>
         </form>
     );
--- 8 - Complete Project/MyImplementation/user-counter/src/components/InputForm/RemoveFormElement/RemoveFormElement.jsx
+++ 8 - Complete Project/MyImplementation/user-counter/src/components/InputForm/RemoveFormElement/RemoveFormElement.jsx
@@ -1,12 +1,36 @@
+const collectFormValues = (e) => {
+    e.preventDefault();
 
-const removeFormFieldElement = (e) => {
+    // normal stuff did not work, please explore how this works if you wish in browser
+    const formProps = Object.keys(e.target)
+                            .slice(0, e.target.length)
+                            .map(idx => e.target[idx]);
+    const formObjects = formProps.map(x => { return {'id': x.id, 'val': x.value}; })
+                                 .slice(0, formProps.length-1);
+
+    return formObjects;
+}
+
+const removeFormFieldElement = (e, formFields, setFormFields) => {
+    e.preventDefault();
     console.log("removeFormFieldElement e:", e);
+    const formValues = collectFormValues(e);
+    console.log('formValues: ', formValues[0].val);
+    console.log('formFields: ', formFields);
+
+    let idxToRemove = formFields.findIndex((field, idx) => {
+        return field.inputId === formValues[0].val;
+    });
+    
+    formFields.splice(idxToRemove, 1);
+
+    setFormFields([...formFields]);
 }
 
 const RemoveFormElement = (props) => {
     return (
         <form
-                onSubmit={(e) => removeFormFieldElement(e)}>
+                onSubmit={(e) => removeFormFieldElement(e, props.formFields, props.setFormFields)}>
                 <div>Remove Field</div>
                 <div>
                     <label htmlFor="InputFieldId">ID:</label>
